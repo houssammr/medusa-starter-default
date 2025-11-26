8338e074a6f343606f9e1e18b9876044c23174ee
houssammr <hmrabihrabbou@gmail.com>
2025-11-26 20:57:57 +0100

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,14 +5,15 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions: {
+      ssl: false,
+    },
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-      // @ts-ignore
-      database_extra: { ssl: false },
     }
   }
 })

6185a6d876c3b6e0492b186a19ba2e348035815f
houssammr <hmrabihrabbou@gmail.com>
2025-11-26 18:08:26 +0100

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -9,6 +9,7 @@ module.exports = defineConfig({
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
+      database_extra: { ssl: false },
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
